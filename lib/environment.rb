30ccd7ff4a9a2eb0416ec9e1bd3cfc15f789a59d
Trevis McGill <trevismcgill25@gmail.com>
2020-08-17 20:33:31 +0000

    JSON to CSV Attempt

--- lib/environment.rb
+++ lib/environment.rb
@@ -3,6 +3,7 @@ require "./lib/lor_project/cli_controller"
 require "./lib/lor_project/api"
 require "./lib/lor_project/card"
 require "./lib/lor_project/deck"
+require "./lib/lor_project/set1-en_us"
 
 # module LorProject
 #   class Error < StandardError; end
--- lib/lor_project/api.rb
+++ lib/lor_project/api.rb
@@ -1,3 +1,18 @@
+require "rest-client"
+require "json"
+require "csv"
+require "pry"
+
 class Api
+
+def self.json_to_csv
+csv_string = CSV.generate do |csv|
+  JSON.parse(File.open("set1-en_us.json").read).each do |hash|
+    csv << hash.values
+    binding.pry
+    end
+  end
+  csv_string
+end
 
 end
